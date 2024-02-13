12c828aa4be5024b54b696cb6588e848d7c377bf
Vincent Hengel <prelink835@gmail.com>
2024-02-13 18:15:49 +0100

    inventory related reverse

--- include/RED4ext/Scripting/Natives/Generated/game/Inventory.hpp
+++ include/RED4ext/Scripting/Natives/Generated/game/Inventory.hpp
@@ -18,15 +18,20 @@ struct Inventory : game::Component
     static constexpr const char* NAME = "gameInventory";
     static constexpr const char* ALIAS = "Inventory";
 
-    uint8_t unkA8[0xB0 - 0xA8]; // A8
+    //WeakHandle<game::Object> owner;
+    EntityID ownerEntityID; // 90
+    //uint8_t unkA8[0xB0 - 0xA8]; // A8
+    //uint8_t unkA8[0xB0 - 16 - 8]; // A8
     game::SharedInventoryTag inventoryTag; // B0
     bool noInitialization; // B4
-    uint8_t unkB5[0xB6 - 0xB5]; // B5
+    bool isIllegaltoLoot; // B5
     bool saveInventory; // B6
     uint8_t unkB7[0x110 - 0xB7]; // B7
 };
 RED4EXT_ASSERT_SIZE(Inventory, 0x110);
 } // namespace game
+
+//constexpr auto x  = sizeof(game::Inventory);
 using gameInventory = game::Inventory;
 using Inventory = game::Inventory;
 } // namespace RED4ext
--- include/RED4ext/Scripting/Natives/Generated/game/ItemData.hpp
+++ include/RED4ext/Scripting/Natives/Generated/game/ItemData.hpp
@@ -17,7 +17,22 @@ struct ItemData : IScriptable
     static constexpr const char* NAME = "gameItemData";
     static constexpr const char* ALIAS = NAME;
 
-    uint8_t unk40[0x78 - 0x40]; // 40
+    virtual ~ItemData() = 0;
+
+    virtual void m1() = 0;
+    virtual void m2() = 0;
+
+    virtual void m3() = 0;
+    virtual void m4() = 0;
+
+    virtual bool IsStackable() = 0;
+    virtual void SetQuantity(uint32_t value) = 0;
+    virtual void ModifyQuantity(int32_t amount) = 0;
+    virtual uint32_t GetQuantity() = 0;
+
+    ItemID itemID; // 40
+
+    uint8_t unk40[0x78 - 0x40 - sizeof(ItemID)]; // 40
 };
 RED4EXT_ASSERT_SIZE(ItemData, 0x78);
 } // namespace game
