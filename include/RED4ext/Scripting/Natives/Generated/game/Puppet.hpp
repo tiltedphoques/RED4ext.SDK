7ac4bbb323a422fa0a611139257fe7d450d83b5f
maximegmd <672982+maximegmd@users.noreply.github.com>
2024-02-17 18:42:32 +0100

    feat: puppet's move component

--- include/RED4ext/Scripting/Natives/Generated/game/Puppet.hpp
+++ include/RED4ext/Scripting/Natives/Generated/game/Puppet.hpp
@@ -4,22 +4,12 @@
 
 // This file is generated from the Game's Reflection data
 
-#include <cstdint>
-#include <RED4ext/Common.hpp>
-#include <RED4ext/Scripting/Natives/Generated/game/PuppetBase.hpp>
+#include <RED4Ext/Scripting/Natives/entPuppet.hpp>
 
 namespace RED4ext
 {
 namespace game
 {
-struct Puppet : game::PuppetBase
-{
-    static constexpr const char* NAME = "gamePuppet";
-    static constexpr const char* ALIAS = NAME;
-
-    uint8_t unk268[0x5F8 - 0x268]; // 268
-};
-RED4EXT_ASSERT_SIZE(Puppet, 0x5F8);
 } // namespace game
 using gamePuppet = game::Puppet;
 } // namespace RED4ext
