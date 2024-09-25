bd9008f3a073fc188824c037e6d53e6f14455e0c
maximegmd <672982+maximegmd@users.noreply.github.com>
2024-09-25 17:18:15 +0200

    Update moveComponent.hpp

--- include/RED4ext/Scripting/Natives/moveComponent.hpp
+++ include/RED4ext/Scripting/Natives/moveComponent.hpp
@@ -13,16 +13,32 @@ namespace RED4ext
 {
 namespace move
 {
+struct Component;
+struct MovementRepresentation
+{
+    Component* parent;
+    bool active;
+    DynArray<void*> stack;
+    int32 activeIndex;
+    void* activeEntry;
+};
+
 struct __declspec(align(0x10)) Component : ent::IMoverComponent
 {
     static constexpr const char* NAME = "moveComponent";
     static constexpr const char* ALIAS = NAME;
 
-    uint8_t unk90[0x220 - 0x90]; // 90
+    uint8_t unk90[0x138 - 0x90]; // 90
+    MovementRepresentation representation;
+    uint8_t unk1C8[0x220 - 0x168]; // 168
     Vector3 speed; // 220
     uint8_t unk22C[0x2C0 - 0x22C]; // 22C
 };
 } // namespace move
 } // namespace RED4ext
+
+RED4EXT_ASSERT_SIZE(RED4ext::move::MovementRepresentation, 0x30);
+RED4EXT_ASSERT_OFFSET(RED4ext::move::MovementRepresentation, activeEntry, 0x28);
+RED4EXT_ASSERT_OFFSET(RED4ext::move::Component, speed, 0x220);
 
 // clang-format on
